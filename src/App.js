70cb923fdb1dd65cc4a3bdc1da1fbff252f835c4
OsuwoJr <johnfredosuwo@gmail.com>
2024-06-29 19:06:20 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -14,27 +14,9 @@ function App() {
   const [posts, setPosts] = useState([
     {
       id: 1,
-      title: "My First Post",
+      title: "Crazy Ex!",
       datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 2,
-      title: "My 2nd Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 3,
-      title: "My 3rd Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 4,
-      title: "My Fourth Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+      body: "After breaking up with Mike, Sarah began receiving creepy messages and photos from unknown numbers, escalating to a video showing her inside her home. Fearing for her safety, she stayed with a friend and suspected Mike might be behind the harassment. Confronting him at his apartment, she discovered a shrine of photos and stolen belongings dedicated to her. Mike admitted he couldn't handle the breakup and had been stalking her. Sarah called the police, who arrested him, and she moved to a new city, learning to trust her instincts and recognize red flags."
     }
   ])
   const [search, setSearch] = useState('');
